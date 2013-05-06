9f8f07fa27ea3585f3261669082c215ded6c56f9
Joshua Jackson <jjackson@kallisteconsulting.com>
2013-05-06 13:15:05 -0500

    Attack MDWT with comments and better variable names

--- lib/src/mdwt_r.c
+++ lib/src/mdwt_r.c
@@ -68,18 +68,18 @@ Change History: Fixed the code such that 1D vectors passed to it can be in
 // This macro is evil because it uses the variable m which is not included in the arguments
 #define mat(a, i, j) (*(a + (m*(j)+i)))  /* macro for matrix indices */
 
-void fpsconv(double *x_in, int lx, double *h0, double *h1, int lhm1, double *x_outl, double *x_outh) {
+void fpsconv(double *x_in, int lx, double *h0, double *h1, int lh_minus_one, double *x_outl, double *x_outh) {
   int i, j, ind;
   double x0, x1;
-  for (i=lx; i<lx+lhm1; i++)
+  for (i=lx; i<lx+lh_minus_one; i++)
     x_in[i] = *(x_in+(i-lx));
   ind = 0;
   for (i=0; i<(lx); i+=2) {
     x0 = 0;
     x1 = 0;
-    for (j=0; j<=lhm1; j++) {
-      x0 = x0 + x_in[i+j] * h0[lhm1-j];
-      x1 = x1 + x_in[i+j] * h1[lhm1-j];
+    for (j=0; j<=lh_minus_one; j++) {
+      x0 = x0 + x_in[i+j] * h0[lh_minus_one-j];
+      x1 = x1 + x_in[i+j] * h1[lh_minus_one-j];
     }
     x_outl[ind] = x0;
     x_outh[ind++] = x1;
@@ -87,75 +87,87 @@ void fpsconv(double *x_in, int lx, double *h0, double *h1, int lhm1, double *x_o
 }
 
 
+void mdwt_allocate(int m, int n, int lh, double **xdummy, double **y_dummy_low, double **y_dummy_high, double **h0, double **h1) {
+  *xdummy       = (double *) rwt_calloc(max(m,n)+lh-1, sizeof(double));
+  *y_dummy_low  = (double *) rwt_calloc(max(m,n),      sizeof(double));
+  *y_dummy_high = (double *) rwt_calloc(max(m,n),      sizeof(double));
+  *h0           = (double *) rwt_calloc(lh,            sizeof(double));
+  *h1           = (double *) rwt_calloc(lh,            sizeof(double));
+}
+
+
 void MDWT(double *x, int m, int n, double *h, int lh, int L, double *y) {
-  double  *h0, *h1, *ydummyl, *ydummyh, *xdummy;
-  long i;
-  int actual_L, actual_m, actual_n, r_o_a, c_o_a, ir, ic, lhm1;
-  xdummy  = (double *) rwt_calloc(max(m,n)+lh-1, sizeof(double));
-  ydummyl = (double *) rwt_calloc(max(m,n),      sizeof(double));
-  ydummyh = (double *) rwt_calloc(max(m,n),      sizeof(double));
-  h0 = (double *) rwt_calloc(lh, sizeof(double));
-  h1 = (double *) rwt_calloc(lh, sizeof(double));
+  double  *h0, *h1, *y_dummy_low, *y_dummy_high, *xdummy;
+  int i, actual_L, lh_minus_one;
+  int upsampled_rows, upsampled_columns, pass_rows, pass_columns, idx_rows, idx_columns;
   
+  mdwt_allocate(m, n, lh, &xdummy, &y_dummy_low, &y_dummy_high, &h0, &h1);
+
   /* analysis lowpass and highpass */
   if (n==1) {
     n = m;
     m = 1;
   }
+  /* h0 <- reversed h
+     h1 <- forward h, even values are sign reversed */
   for (i=0; i<lh; i++) {
-    h0[i] = h[lh-i-1];
+    h0[i] = h[(lh-i)-1];
     h1[i] = h[i];
   }
   for (i=0; i<lh; i+=2)
     h1[i] = -h1[i];
   
-  lhm1 = lh - 1;
-  actual_m = 2*m;
-  actual_n = 2*n;
-  
+  lh_minus_one = lh - 1;
+  upsampled_rows = 2*m;
+  upsampled_columns = 2*n;
+ 
+  //mexPrintf("new signal. n is %d\n", n);
+ 
   /* main loop */
   for (actual_L=1; actual_L <= L; actual_L++) {
     if (m==1)
-      actual_m = 1;
+      upsampled_rows = 1;
     else{
-      actual_m = actual_m/2;
-      r_o_a = actual_m/2;     
+      upsampled_rows = upsampled_rows/2;
+      pass_rows = upsampled_rows/2;     
     }
-    actual_n = actual_n/2;
-    c_o_a = actual_n/2;
-    
+    upsampled_columns = upsampled_columns/2;
+    pass_columns = upsampled_columns/2;
+
+    //mexPrintf("1d: %d %d\n", upsampled_columns, pass_columns);
     /* go by rows */
-    for (ir=0; ir<actual_m; ir++) {            /* loop over rows */
+    for (idx_rows=0; idx_rows<upsampled_rows; idx_rows++) {            /* loop over rows */
       /* store in dummy variable */
 // Why do we copy in and out of dummy vars?
-      for (i=0; i<actual_n; i++)
+      for (i=0; i<upsampled_columns; i++)
 	if (actual_L==1)  
-	  xdummy[i] = mat(x, ir, i);  
+	  xdummy[i] = mat(x, idx_rows, i);  
 	else 
-	  xdummy[i] = mat(y, ir, i);  
+	  xdummy[i] = mat(y, idx_rows, i);  
       /* perform filtering lowpass and highpass*/
-      fpsconv(xdummy, actual_n, h0, h1, lhm1, ydummyl, ydummyh); 
+      fpsconv(xdummy, upsampled_columns, h0, h1, lh_minus_one, y_dummy_low, y_dummy_high); 
       /* restore dummy variables in matrices */
-      ic = c_o_a;
-      for  (i=0; i<c_o_a; i++) {    
-	mat(y, ir, i) = ydummyl[i];  
-	mat(y, ir, ic++) = ydummyh[i];  
+      idx_columns = pass_columns;
+      for  (i=0; i<pass_columns; i++) {    
+	mat(y, idx_rows, i) = y_dummy_low[i];  
+	mat(y, idx_rows, idx_columns++) = y_dummy_high[i];  
       } 
     }  
     
     /* go by columns in case of a 2D signal*/
     if (m>1) {
-      for (ic=0; ic<actual_n; ic++) {            /* loop over column */
+      //mexPrintf("2d: %d %d %d %d\n", upsampled_rows, pass_rows, upsampled_columns, pass_columns);
+      for (idx_columns=0; idx_columns<upsampled_columns; idx_columns++) { /* loop over columns */
 	/* store in dummy variables */
-	for (i=0; i<actual_m; i++)
-	  xdummy[i] = mat(y, i, ic);  
+	for (i=0; i<upsampled_rows; i++)
+	  xdummy[i] = mat(y, i, idx_columns);  
 	/* perform filtering lowpass and highpass*/
-	fpsconv(xdummy, actual_m, h0, h1, lhm1, ydummyl, ydummyh); 
+	fpsconv(xdummy, upsampled_rows, h0, h1, lh_minus_one, y_dummy_low, y_dummy_high); 
 	/* restore dummy variables in matrix */
-	ir = r_o_a;
-	for (i=0; i<r_o_a; i++) {
-	  mat(y, i, ic) = ydummyl[i];  
-	  mat(y, ir++, ic) = ydummyh[i];  
+	idx_rows = pass_rows;
+	for (i=0; i<pass_rows; i++) {
+	  mat(y, i, idx_columns) = y_dummy_low[i];  
+	  mat(y, idx_rows++, idx_columns) = y_dummy_high[i];  
 	}
       }
     }
--- tests/test_mdwt.m
+++ tests/test_mdwt.m
@@ -1,28 +1,21 @@
 function test_suite = test_mdwt
 initTestSuite;
 
-
-
 function test_mdwt_1D
        x = makesig('LinChirp',8);
        h = daubcqf(4,'min');
-       L = 2;
+       L = 2;  % For 8 values in x we would normally be L=2 
        [y,L] = mdwt(x,h,L);
-
        y_corr = [1.1097 0.8767 0.8204 -0.5201 -0.0339 0.1001 0.2201 -0.1401];
        L_corr = 2;
-
 assertVectorsAlmostEqual(y, y_corr,'relative',0.001);
 assertEqual(L,L_corr);
-
-
 
 function test_mdwt_compute_L1
        x = [1 2];
        h = daubcqf(4,'min');
        [~,L] = mdwt(x,h);
 assertEqual(L,1);
-
 
 function test_mdwt_compute_L2
        x = [1 2 3 4];
@@ -43,5 +36,3 @@ function test_mdwt_compute_bad_L
        
        mdwtHandle = @() mdwt(x,h);
 assertExceptionThrown(mdwtHandle,'') ;
-
-
