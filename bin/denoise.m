2896c95dfdad20d28815510078400025dadf7862
Joshua Jackson <jjackson@kallisteconsulting.com>
2013-06-14 18:26:28 -0500

    Remove old license info

--- bin/denoise.m
+++ bin/denoise.m
@@ -78,60 +78,7 @@ function [xd,xn,option] = denoise(x,h,type,option)
 %
 %    See also: mdwt, midwt, mrdwt, mirdwt, SoftTh, HardTh, setopt
 %
-
-%File Name: denoise.m
-%Last Modification Date: 04/15/97	10:44:28
-%Current Version: denoise.m	2.4
-%File Creation Date: Mon Feb 20 08:33:15 1995
 %Author: Jan Erik Odegard  <odegard@ece.rice.edu>
-%
-%Copyright (c) 2000 RICE UNIVERSITY. All rights reserved.
-%Created by Jan Erik Odegard, Department of ECE, Rice University. 
-%
-%This software is distributed and licensed to you on a non-exclusive 
-%basis, free-of-charge. Redistribution and use in source and binary forms, 
-%with or without modification, are permitted provided that the following 
-%conditions are met:
-%
-%1. Redistribution of source code must retain the above copyright notice, 
-%   this list of conditions and the following disclaimer.
-%2. Redistribution in binary form must reproduce the above copyright notice, 
-%   this list of conditions and the following disclaimer in the 
-%   documentation and/or other materials provided with the distribution.
-%3. All advertising materials mentioning features or use of this software 
-%   must display the following acknowledgment: This product includes 
-%   software developed by Rice University, Houston, Texas and its contributors.
-%4. Neither the name of the University nor the names of its contributors 
-%   may be used to endorse or promote products derived from this software 
-%   without specific prior written permission.
-%
-%THIS SOFTWARE IS PROVIDED BY WILLIAM MARSH RICE UNIVERSITY, HOUSTON, TEXAS, 
-%AND CONTRIBUTORS AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
-%BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-%FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RICE UNIVERSITY 
-%OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-%EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-%PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-%OR BUSINESS INTERRUPTIONS) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-%WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-%OTHERWISE), PRODUCT LIABILITY, OR OTHERWISE ARISING IN ANY WAY OUT OF THE 
-%USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-%
-%For information on commercial licenses, contact Rice University's Office of 
-%Technology Transfer at techtran@rice.edu or (713) 348-6173
-%
-%Change History: Fixed output of function and an error in the computation
-%                of the threshold for redundant denoising.
-%                <Jan Erik Odegard> <Mon Jul 31, 1995>
-%
-%                This code is composed of several of our old codes for
-%                wavelet based denoising. In an effort to make the mess
-%                more manageable we decided to create on code that would 
-%                handle all the various wavelet based denoising methods.
-%                However, only time will show (as we discover new and 
-%                improved forms of denoising) if we can succeed in our goals.
-%                <Jan Erik Odegard> <Thu May 11, 1995>
-%
 
 if(nargin < 2)
   error('You need to provide at least 2 inputs: x and h');
--- bin/makesig.m
+++ bin/makesig.m
@@ -33,52 +33,10 @@ function [x,N] = makesig(SigName,N)
 %            www_url: http://playfair.stanford.edu/~wavelab/
 %            Also see various articles by D.L. Donoho et al. at
 %            web_url: http://playfair.stanford.edu/
-
-%File Name: makesig.m
-%Last Modification Date: 08/30/95	15:52:03
-%Current Version: makesig.m	2.4
-%File Creation Date: Thu Jun  8 10:31:11 1995
+%
 %Author: Jan Erik Odegard  <odegard@ece.rice.edu>
-%
-%Copyright (c) 2000 RICE UNIVERSITY. All rights reserved.
-%Created by Jan Erik Odegard, Department of ECE, Rice University. 
-%
-%This software is distributed and licensed to you on a non-exclusive 
-%basis, free-of-charge. Redistribution and use in source and binary forms, 
-%with or without modification, are permitted provided that the following 
-%conditions are met:
-%
-%1. Redistribution of source code must retain the above copyright notice, 
-%   this list of conditions and the following disclaimer.
-%2. Redistribution in binary form must reproduce the above copyright notice, 
-%   this list of conditions and the following disclaimer in the 
-%   documentation and/or other materials provided with the distribution.
-%3. All advertising materials mentioning features or use of this software 
-%   must display the following acknowledgment: This product includes 
-%   software developed by Rice University, Houston, Texas and its contributors.
-%4. Neither the name of the University nor the names of its contributors 
-%   may be used to endorse or promote products derived from this software 
-%   without specific prior written permission.
-%
-%THIS SOFTWARE IS PROVIDED BY WILLIAM MARSH RICE UNIVERSITY, HOUSTON, TEXAS, 
-%AND CONTRIBUTORS AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
-%BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-%FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RICE UNIVERSITY 
-%OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-%EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-%PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-%OR BUSINESS INTERRUPTIONS) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-%WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-%OTHERWISE), PRODUCT LIABILITY, OR OTHERWISE ARISING IN ANY WAY OUT OF THE 
-%USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-%
-%For information on commercial licenses, contact Rice University's Office of 
-%Technology Transfer at techtran@rice.edu or (713) 348-6173
-%
-%Change History: This m-file is a copy of the  code provided with WaveLab
-%                customized to be consistent with RWT.
-%                Jan Erik Odegard <odegard@ece.rice.edu> Thu Jun  8 1995
-%
+%This m-file is a copy of the  code provided with WaveLab
+%customized to be consistent with RWT.
 
 if(nargin < 1)
   SigName = 'AllSig';
