c1512432be76fcbf8d7f4071f2ceafac6b260514
Joshua Jackson <jjackson@kallisteconsulting.com>
2013-09-23 16:34:53 -0500

    Tests for Octave

--- octave/build.sh
+++ octave/build.sh
@@ -1,4 +1,3 @@
 mkoctfile --mex -v ../src/mdwt.c ../lib/src/dwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
 mkoctfile --mex -v ../src/midwt.c ../lib/src/idwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
-mkoctfile --mex -v ../src/mrdwt.c ../lib/src/rdwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
-mkoctfile --mex -v ../src/mirdwt.c ../lib/src/irdwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
+
---
+++ octave/runtests.m
@@ -0,0 +1,9 @@
+path('../octave', '../bin', path)
+%path(path, 'matlab_xunit/xunit')
+test_mdwt
+test_midwt
+%test_mirdwt
+%test_mrdwt
+test_makesig
+test_denoise
+test_setopt
