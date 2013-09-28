30afa23a64034a09eb11fc3f68af07a4fd8ce8c6
Joshua Jackson <jjackson@kallisteconsulting.com>
2013-09-28 16:21:55 -0500

    src directory becomes mex directory

--- bin/compile.m
+++ bin/compile.m
@@ -1,7 +1,7 @@
 %    COMPILE compiles the c files and generates mex files.
 %
 
-mex -v ../src/mdwt.c ../lib/src/dwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
-mex -v ../src/midwt.c ../lib/src/idwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
-mex -v ../src/mrdwt.c ../lib/src/rdwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
-mex -v ../src/mirdwt.c ../lib/src/irdwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
+mex -v ../mex/mdwt.c ../lib/src/dwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
+mex -v ../mex/midwt.c ../lib/src/idwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
+mex -v ../mex/mrdwt.c ../lib/src/rdwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
+mex -v ../mex/mirdwt.c ../lib/src/irdwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
--- octave/compile.m
+++ octave/compile.m
@@ -1,2 +1,2 @@
-mkoctfile --mex -v ../src/mdwt.c ../lib/src/dwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
-mkoctfile --mex -v ../src/midwt.c ../lib/src/idwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
+mkoctfile --mex -v ../mex/mdwt.c ../lib/src/dwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
+mkoctfile --mex -v ../mex/midwt.c ../lib/src/idwt.c ../lib/src/init.c ../lib/src/platform.c -I../lib/inc 
